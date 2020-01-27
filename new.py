e8a3ac4daa78e7c1fb4f5e4830e3486fbb924490
SergeyVegero <60273180+SergeyVegero@users.noreply.github.com>
2020-01-27 21:01:36 +0200

    Add files via upload

---
+++ new.py
@@ -0,0 +1,22 @@
+q, w, e, r = 1, 2, 3, 4
+print(q+r); print(w*r); print(r/w); print(e*r)
+
+
+
+a= "Hello"
+s= " World!"
+print(a+s)
+
+import random
+list = [11, 22, 33, 44, 55, 66, 77, 88, 99, 00]
+print ("random.sample() ", random.sample(list,9))
+
+random.sample(range(10), 10)
+a=[random.randint(0,50) for i in range(10)]
+print(a.pop())
+print(a)
+
+k = [random.randint(1, 50)
+for i in range(10)]
+del k [-1]
+print(k)
